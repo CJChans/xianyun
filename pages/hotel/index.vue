9bb583582c4d1977f9c318a6f4e650875eed3009
CJChans <52377346+CJChans@users.noreply.github.com>
2019-10-21 14:04:30 +0800

    59-地图添加点标记

--- pages/hotel/index.vue
+++ pages/hotel/index.vue
@@ -9,7 +9,16 @@
 export default {
     mounted(){
           window.onLoad  = function(){
-            var map = new AMap.Map('container');
+             var map = new AMap.Map('container',{
+                  zoom:14,//级别
+                  center: [113.3245904, 23.1066805],//中心点坐标
+                  viewMode:'3D'//使用3D视图
+            });
+
+            var marker = new AMap.Marker({
+                position:[113.3245904, 23.1066805]//位置
+            })
+            map.add(marker);//添加到地图
       }
           var url = 'https://webapi.amap.com/maps?v=1.4.15&0bb89d19d60f6b6c21251c9751c72444 &callback=onLoad';
           var jsapi = document.createElement('script');
